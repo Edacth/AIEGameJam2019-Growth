76325fcadd874c754c98c8d29532b5b334b12920
Cade Anderson <cadeanderson99@gmail.com>
2019-07-24 17:28:03 -0700

    Test out rough dragging mechanics and object physics

---
+++ Assets/Scripts/DragController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragController : MonoBehaviour {
+    private float screenHeightInUnits;
+    private float screenWidthInUnits;
+    private float unitPerPixel;
+    public Vector2 mousePos;
+    public Vector2 cachedMousePos;
+    LayerMask mask;
+    public Vector3 dragDir;
+
+
+    public Rigidbody selectedRigidBody;
+    public GameObject testObject;
+    public Camera mainCamera;
+
+    void Start ()
+    {
+        screenHeightInUnits = mainCamera.orthographicSize * 2;
+        screenWidthInUnits = screenHeightInUnits * ((float)Screen.width / (float)Screen.height);
+        unitPerPixel = screenHeightInUnits / Screen.height;
+        mask = LayerMask.GetMask("Draggable");
+    }
+	
+	void Update ()
+    {
+        mousePos = Input.mousePosition;
+        Vector3 mouseWorldPosition = new Vector3(mousePos.x * unitPerPixel, 5, mousePos.y * unitPerPixel);
+        Debug.DrawRay(mouseWorldPosition, Vector3.down * 10, Color.red);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mouseWorldPosition, Vector3.down * 10, out hit, 10, mask))
+            {
+                selectedRigidBody = hit.rigidbody;
+                Debug.DrawRay(mouseWorldPosition, Vector3.down * 10, Color.green);
+                Debug.Log("Found RB");
+            }
+            cachedMousePos = mousePos;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectedRigidBody = null;
+            cachedMousePos = Vector2.zero;
+        }
+
+        if (cachedMousePos != Vector2.zero)
+        {
+            //if (mousePos.x > cachedMousePos.x)
+            //{
+            //    dragDir.x = 1;
+            //}
+            //else if (mousePos.x < cachedMousePos.x)
+            //{
+            //    dragDir.x = -1;
+            //}
+
+            //if (mousePos.y > cachedMousePos.y)
+            //{
+            //    dragDir.z = 1;
+            //}
+            //else if (mousePos.y < cachedMousePos.y)
+            //{
+            //    dragDir.z = -1;
+            //}
+
+            dragDir.x = mousePos.x - cachedMousePos.x;
+            dragDir.z = mousePos.y - cachedMousePos.y;
+        }
+
+        if (selectedRigidBody != null)
+        {
+            selectedRigidBody.AddForce(dragDir * 2);
+            cachedMousePos = mousePos;
+        }
+	}
+}
---
+++ Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+    public Vector3 movement;
+    public float speed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        movement = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        movement = Vector3.zero;
+        movement = new Vector3(Input.GetAxis("Horizontal") * speed, 0 , Input.GetAxis("Vertical") * speed);
+        if (movement != Vector3.zero)
+        {
+            rb.AddForce(movement);
+        }
+	}
+}
